7dfaa43de654afae2adcaa924055817651cb113e
tyrus2157 <82844198+tyrus2157@users.noreply.github.com>
2025-03-11 11:02:01 +0100

    Update labyrinthe pour qu'il soit en accord avec le render

--- labyrinthe.py
+++ labyrinthe.py
@@ -11,19 +11,22 @@ class Case:
         self.murW = True
         self.murE = True
         self.vue = False
-
+        self.est_sortie = False  # Attribut pour indiquer si la case est une sortie
+        self.direction_sortie = None  # Direction de la sortie (N, S, E, W)
 
 class Labyrinthe:
     def __init__(self, largeur, hauteur):
         self.largeur = largeur
         self.hauteur = hauteur
         self.laby = []
+        self.sortie = None  # Pour stocker les coordonnées de la sortie
+        
         for ligne in range(self.largeur):
             self.laby.append([])
             for colonne in range(self.hauteur):
                 colonne = Case()
                 self.laby[ligne].append(colonne)
-
+    
     def __directions_possibles(self, i, j):
         directions = []
         if j < self.hauteur - 1 and not self.laby[i][j + 1].vue:
@@ -35,7 +38,7 @@ class Labyrinthe:
         if i > 0 and not self.laby[i - 1][j].vue:
             directions.append('W')
         return directions
-
+    
     def __abattre_mur(self, i, j, dir, pile):
         if dir == 'S':
             self.laby[i][j].murS = False
@@ -57,33 +60,84 @@ class Labyrinthe:
             self.laby[i + 1][j].murW = False
             self.laby[i + 1][j].vue = True
             pile.empiler((i + 1, j))
-
+    
     def generer(self):
+        # Marquer toutes les cases comme non visitées
+        for i in range(self.largeur):
+            for j in range(self.hauteur):
+                self.laby[i][j].vue = False
+                self.laby[i][j].est_sortie = False
+                self.laby[i][j].direction_sortie = None
+                
         pile = Pile()
         x = randint(0, self.largeur - 1)
         y = randint(0, self.hauteur - 1)
+        self.laby[x][y].vue = True  # Marquer la première case comme visitée
         pile.empiler((x, y))
+        
         while not pile.est_vide():
             case = pile.depiler()
             direction = self.__directions_possibles(case[0], case[1])
-            if len(direction) > 1:
+            if len(direction) > 0:
                 pile.empiler((case[0], case[1]))
-            if direction:
                 direction_choose = choice(direction)
                 self.__abattre_mur(case[0], case[1], direction_choose, pile)
-
+    
+    def generer_sortie(self):
+        """Génère une sortie aléatoire sur le bord du labyrinthe.
+        Retourne les coordonnées de la sortie sous forme de tuple (i, j) et la direction ('N', 'S', 'E', 'W')."""
+        
+        # Choisir aléatoirement un côté du labyrinthe
+        cote = choice(['N', 'S', 'E', 'W'])
+        
+        if cote == 'N':  # Mur Nord
+            i = randint(0, self.largeur - 1)
+            j = 0
+            self.laby[i][j].murN = False
+            self.laby[i][j].est_sortie = True
+            self.laby[i][j].direction_sortie = 'N'
+            self.sortie = (i, j)
+            return (i, j), 'N'
+        
+        elif cote == 'S':  # Mur Sud
+            i = randint(0, self.largeur - 1)
+            j = self.hauteur - 1
+            self.laby[i][j].murS = False
+            self.laby[i][j].est_sortie = True
+            self.laby[i][j].direction_sortie = 'S'
+            self.sortie = (i, j)
+            return (i, j), 'S'
+        
+        elif cote == 'W':  # Mur Ouest
+            i = 0
+            j = randint(0, self.hauteur - 1)
+            self.laby[i][j].murW = False
+            self.laby[i][j].est_sortie = True
+            self.laby[i][j].direction_sortie = 'W'
+            self.sortie = (i, j)
+            return (i, j), 'W'
+        
+        elif cote == 'E':  # Mur Est
+            i = self.largeur - 1
+            j = randint(0, self.hauteur - 1)
+            self.laby[i][j].murE = False
+            self.laby[i][j].est_sortie = True
+            self.laby[i][j].direction_sortie = 'E'
+            self.sortie = (i, j)
+            return (i, j), 'E'
+    
     def get_murs(self):
+        """Retourne la liste des murs dans le format attendu par la fonction render."""
         murs = []
         for i in range(self.largeur):
             for j in range(self.hauteur):
-                murs.append(((i, j), self.laby[i][j].murN, self.laby[i][j].murS, self.laby[i][j].murW, self.laby[i][j].murE))
+                murs.append(((i, j), self.laby[i][j].murN, self.laby[i][j].murS, 
+                           self.laby[i][j].murW, self.laby[i][j].murE))
         return murs
-
-
-### Programme principal ###
-if __name__ == '__main__':
-    lab = Labyrinthe(100, 100)
-    lab.generer()
-    murs = lab.get_murs()
-    for mur in murs:
-        print(mur)
+    
+    def get_sortie(self):
+        """Retourne les informations sur la sortie."""
+        if self.sortie:
+            i, j = self.sortie
+            return (i, j), self.laby[i][j].direction_sortie
+        return None, None
